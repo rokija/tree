ca706903f8430e22bb05d33ff7e669f831b998f7
Ruta <ruta.freimane@accenture.com>
2019-07-03 17:31:47 +0300

    created tree using recursive approach

--- src/components/Tree/index.js
+++ src/components/Tree/index.js
@@ -1,59 +1,85 @@
-import React, { Component, Fragment } from 'react'
-import Leaf from '../Leaf'
+import React, { Component } from 'react'
+import './Tree.less'
+
+// if nested data is given
+const rootTree = {
+  name: 'Lorem',
+  id: 'root',
+  children: [
+    { name: 'Ipsum', id: 'root-child', children: [] },
+    { name: 'Dolor', id: 'root-child-2', children: [] },
+  ],
+}
 
 class Tree extends Component {
     state = {
-      inputValue: '',
-      selectedNodeIndex: null,
-      children: [{
-        id: 1,
-        parentId: 0,
-        value: 'root leaf',
-        paddingLeft: 10,
-      }],
+      tree: rootTree,
+      showInput: false,
+      value: null,
+      selectedId: null,
     }
 
-    onInputChange = e => this.setState({ inputValue: e.target.value })
+  renderTree = leaf => (
+    <li key={leaf.id} data-id={leaf.id}>
+      {leaf.name}
+      {!!leaf.children.length
+        && (
+          <ul>
+            {leaf.children.map(child => this.renderTree(child))}
+          </ul>
+        )
+      }
+    </li>
+  )
 
-    onOpenInput = index => this.setState({ selectedNodeIndex: index })
+  addLeaf = (leaf, id) => {
+    const { tree } = this.state
 
-    onAddNode = (id) => {
-      const { children, inputValue } = this.state
-      const parentElement = children.find(child => child.id === id)
+    const traverseTree = (node) => {
+      if (node.id === id) {
+        node.children.push(leaf)
+      }
+      if (!node.children.length) {
+        return tree
+      }
 
-      this.setState({
-        children: [...children, {
-          id: id + 1, parentId: id, value: inputValue, paddingLeft: parentElement.paddingLeft + 10,
-        }],
-        inputValue: '',
-        selectedNodeIndex: null,
-      })
+      node.children.forEach(child => traverseTree(child))
+      return node
     }
 
-    render() {
-      const { children, selectedNodeIndex } = this.state
+    this.setState({ tree: traverseTree(tree), showInput: false, value: null })
+  }
 
-      return (
-        <div>
-          {children.map((leaf, i) => (
-            <div key={`leaf-${i}`}>
-              <Leaf
-                index={i}
-                paddingLeft={leaf.paddingLeft}
-                value={leaf.value}
-                onOpenInput={this.onOpenInput}
-              />
-              {selectedNodeIndex === i && (
-                <Fragment>
-                  <input onChange={this.onInputChange} />
-                  <button onClick={() => this.onAddNode(leaf.id)}>+</button>
-                </Fragment>
-              )}
-            </div>
-          ))}
-        </div>
-      )
-    }
+  onInputChange = e => this.setState({ value: e.target.value })
+
+  onTreeClick = (event) => {
+    event.persist()
+    this.setState(prevState => ({ showInput: !prevState.showInput, selectedId: event.target.dataset.id }))
+  }
+
+
+  render() {
+    const {
+      tree, showInput, value, selectedId,
+    } = this.state
+
+    const newLeaf = { name: value, id: `${(new Date()).getTime()}`, children: [] }
+
+    return (
+      <div className="Tree">
+        <ul onClick={this.onTreeClick}>
+          {this.renderTree(tree)}
+        </ul>
+        {showInput && (
+          <div>
+            <h2>Leaf Title</h2>
+            <input onChange={this.onInputChange} />
+            <button onClick={() => this.addLeaf(newLeaf, selectedId)}>+</button>
+          </div>
+        )}
+      </div>
+    )
+  }
 }
 
 export default Tree
